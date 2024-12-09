af4099dce1f54744e83343c0fe50e9c418941c93
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-12-09 17:19:47 +0800

    Normalize users slice

--- src/features/users/users-slice.ts
+++ src/features/users/users-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit'
 import { RootState } from '@/app/store'
 import { createAppAsyncThunk } from '@/app/with-types'
 import { client } from '@/api/client'
@@ -10,30 +10,35 @@ interface User {
   username: string
 }
 
+const usersAdapter = createEntityAdapter<User>()
+
 export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
   const response = await client.get<User[]>('/fakeApi/users')
   return response.data
 })
 
-const initialState: User[] = []
+const initialState = usersAdapter.getInitialState()
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      return action.payload
-    })
+    builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
   },
 })
 
 export default usersSlice.reducer
 
-export const selectAllUsers = (state: RootState) => state.users
-export const selectUserById = (state: RootState, userId: string | null) =>
-  state.users.find(user => user.id === userId)
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+} = usersAdapter.getSelectors((state: RootState) => state.users)
+
 export const selectCurrentUser = (state: RootState) => {
-  const username = selectCurrentUsername(state)
-  return state.users.find(user => user.id === username)
+  const currentUsername = selectCurrentUsername(state)
+  if (!currentUsername) {
+    return
+  }
+  return selectUserById(state, currentUsername)
 }
